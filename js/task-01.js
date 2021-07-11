b73f7bf8d82b6ec5dfa1b6f5587938c52043bfbb
Ihor Rudniev <rudnievihor@gmail.com>
2021-07-11 21:15:34 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const navListEl = document.querySelector('#categories');
+console.log(navListEl);
+
+const navItemEl = document.querySelectorAll('.item');
+const quantityElements = `В списке ${navItemEl.length} категории`;
+console.log(quantityElements);
+
+const titleEl = document.querySelectorAll('.item h2');
+console.log(...titleEl);
+
