5464f9073367e6e490f9396c5a8e67a52b8365b8
Ihor Rudniev <rudnievihor@gmail.com>
2021-07-14 20:57:44 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+// console.log(ingredients);
+
+const listEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(element => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = element;
+   
+  return itemEl;
+});
+ 
+listEl.append(...elements);
+
+console.log(listEl);
