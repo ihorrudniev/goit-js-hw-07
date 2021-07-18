25056224174813e82dda6d5f1ebed9ec1eeb9010
Ihor Rudniev <rudnievihor@gmail.com>
2021-07-18 06:21:51 +0300

    Update-task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,39 @@
-const targetBtnEl= document.querySelectorAll('#counter button');
+const targetBtnEl = document.querySelectorAll('#counter button');
+const decrementBtnEl = targetBtnEl[0];
+// console.log(decrementBtnEl);
+const incrementBtnEl = targetBtnEl[1];
+// console.log(incrementBtnEl);
 
 const valueEl = document.querySelector('#value');
+// console.log(valueEl);
 
-// targetBtnDecr.addEventListener('click', () => {
-//     console.log('Удалить число');
-// });
+const counter = {
+  value: 0,
+  decrement() {
+    this.value -= 1;
+  },
+
+  increment() {
+    this.value += 1;
+  },
+};
+
+decrementBtnEl.addEventListener('click', function () {
+  console.log('Click decrement');
+  
+  counter.decrement();
+  // console.log(counter);
+
+  valueEl.textContent = counter.value;
+});
+  
+incrementBtnEl.addEventListener('click', function () {
+  console.log('Click increment');
+  
+  counter.increment();
+  // console.log(counter);
+
+  valueEl.textContent = counter.value;
+});
 
 
-// const decrement () {};
-
-
-console.log(targetBtnEl);
-
-console.log(valueEl);
